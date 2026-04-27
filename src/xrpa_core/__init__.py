6db4a2c3c0573768751ac16ff857e272043e6c3a
xen <yong11021@outlook.com>
2026-04-27 15:19:39 +0800

    feat: include core module in public package interface

--- src/xrpa_core/__init__.py
+++ src/xrpa_core/__init__.py
@@ -1,7 +1,8 @@
 """xrpa_core public package interface."""
 
+import xrpa_core.core as core
 import xrpa_core.feishu as feishu
 
 __version__ = "0.1.0"
 
-__all__ = ["__version__", "feishu"]
+__all__ = ["__version__", "feishu", "core"]
